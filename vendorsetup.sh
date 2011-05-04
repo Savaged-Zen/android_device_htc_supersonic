5f791f9258e6549b6e4871de183ed5def6c278af
Joshua Seidel <jsseidel1@gmail.com>
2011-05-04 06:22:21 -0500

    comment out unused lunchs
    Change-Id: I6b2081e2d278052fde4fa370fa18fca976b93a21

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -20,5 +20,5 @@
 # In particular, you can add lunch options with the add_lunch_combo
 # function: add_lunch_combo generic-eng
 
-add_lunch_combo htc_supersonic-userdebug
-add_lunch_combo htc_supersonic-eng
+#add_lunch_combo htc_supersonic-userdebug
+#add_lunch_combo htc_supersonic-eng
